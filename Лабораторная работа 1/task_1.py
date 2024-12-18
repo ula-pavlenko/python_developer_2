e99058d7c97d9c872dde27d49e99d2794150972a
ula-pavlenko <ulia.pavlenko22319@gmail.com>
2024-12-18 20:58:01 +0300

    Add files via upload

--- Лабораторная работа 1/task_1.py
+++ Лабораторная работа 1/task_1.py
@@ -3,7 +3,7 @@ import doctest
 
 
 class Audience:
-    """ Класс учебная аудитория """
+    """Класс учебная аудитория"""
 
     def __init__(self, number: int, capacity: int):
         """
@@ -16,14 +16,14 @@ class Audience:
         >>> audience = Audience(10, 50)
         """
         if not isinstance(number, int):
-            raise TypeError('Номер аудитории должен быть целым числом')
+            raise TypeError("Номер аудитории должен быть целым числом")
         if number < 0:
-            raise ValueError('Номер аудитории должен быть положительным числом')
+            raise ValueError("Номер аудитории должен быть положительным числом")
 
         if not isinstance(capacity, int):
-            raise TypeError('Вместимость аудитории должна быть целым числом')
+            raise TypeError("Вместимость аудитории должна быть целым числом")
         if capacity <= 0:
-            raise ValueError('Вместимость аудитории должна быть положительным числом')
+            raise ValueError("Вместимость аудитории должна быть положительным числом")
         self.number = number
         self.capacity = capacity
         self.occupied = 0
@@ -39,9 +39,20 @@ class Audience:
         >>> audience.start_lecture(40)
         >>> audience.occupied
         40
+        >>> audience.stop_lecture()
+        >>> audience.start_lecture(60)
+        Traceback (most recent call last):
+        ...
+        ValueError: Количество студентов должно быть в интервале от 0 до 50
         """
         if not isinstance(students_count, int):
-            raise TypeError('Количество студентов должно быть целым числом')
+            raise TypeError("Количество студентов должно быть целым числом")
+
+        if not 0 <= students_count <= self.capacity:
+            raise ValueError(
+                f"Количество студентов должно быть в интервале от 0 до {self.capacity}"
+            )
+
         self.occupied = students_count
 
     def stop_lecture(self) -> None:
@@ -59,7 +70,7 @@ class Audience:
 
 
 class Subject:
-    """ Класс дисциплина обучения """
+    """Класс дисциплина обучения"""
 
     def __init__(self, name: str, duration_hours: int):
         """
@@ -72,14 +83,16 @@ class Subject:
         >>> subject = Subject('Языкознание', 10)
         """
         if not isinstance(name, str):
-            raise TypeError('Название дисциплины должно быть строкой')
+            raise TypeError("Название дисциплины должно быть строкой")
         if not name:
-            raise ValueError('Название дисциплины не может быть пустой строкой')
+            raise ValueError("Название дисциплины не может быть пустой строкой")
 
         if not isinstance(duration_hours, int):
-            raise TypeError('Продолжительность дисциплины должна быть целым числом')
+            raise TypeError("Продолжительность дисциплины должна быть целым числом")
         if duration_hours <= 0:
-            raise ValueError('Продолжительность дисциплины должна быть положительным числом')
+            raise ValueError(
+                "Продолжительность дисциплины должна быть положительным числом"
+            )
 
         self.name = name
         self.duration_hours = duration_hours
@@ -94,11 +107,18 @@ class Subject:
         Примеры:
         >>> subject = Subject('Языкознание', 10)
         >>> subject.learn(5)
+        >>> subject.learn(6)
+        Traceback (most recent call last):
+        ...
+        ValueError: Количество пройденных часов должно быть в интервале от 0 до 5
         """
         if not isinstance(hours, int):
-            raise TypeError('Количество пройденных часов должно быть целым числом')
-        if hours < 0:
-            raise ValueError('Количество пройденных часов должно быть неотрицательным числом')
+            raise TypeError("Количество пройденных часов должно быть целым числом")
+        if not 0 <= hours <= self.remain_hours():
+            raise ValueError(
+                f"Количество пройденных часов должно быть в интервале от 0 до {self.remain_hours()}"
+            )
+
         self.progress_hours += hours
 
     def remain_hours(self) -> int:
@@ -117,7 +137,7 @@ class Subject:
 
 
 class Student:
-    """ Класс студент """
+    """Класс студент"""
 
     def __init__(self, name: str, courses_count: int):
         """
@@ -128,16 +148,16 @@ class Student:
 
         Примеры:
         >>> student = Student('Иван', 3)
-       """
+        """
         if not isinstance(name, str):
-            raise TypeError('Имя студента должно быть строкой')
+            raise TypeError("Имя студента должно быть строкой")
         if not name:
-            raise ValueError('Имя студента не может быть пустой строкой')
+            raise ValueError("Имя студента не может быть пустой строкой")
 
         if not isinstance(courses_count, int):
-            raise TypeError('Количество лет обучения должно быть целым числом')
+            raise TypeError("Количество лет обучения должно быть целым числом")
         if courses_count <= 0:
-            raise ValueError('Количество лет обучения должно быть положительным числом')
+            raise ValueError("Количество лет обучения должно быть положительным числом")
 
         self.name = name
         self.courses_count = courses_count
